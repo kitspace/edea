3a53a682b7a87fc2b0e3a3cee9b823db481f8505
Kaspar Emanuel <kaspar@kitspace.org>
2022-08-10 21:32:33 +0100

    Add assertion that sch file test is not empty

--- tests/test_parse_all_sch_files.py
+++ tests/test_parse_all_sch_files.py
@@ -11,19 +11,21 @@ from edea.edea import Project
 
 test_folder = os.path.dirname(os.path.realpath(__file__))
 kicad_folder = os.path.join(test_folder, "kicad_projects/kicad6-sch-files")
+kicad_sch_files = []
+for root, dirs, files in os.walk(kicad_folder):
+    # don't go into any .git directories.
+    if ".git" in dirs:
+        dirs.remove(".git")
 
+    for file in files:
+        if file.endswith(".kicad_sch"):
+            path = os.path.join(root, file)
+            kicad_sch_files.append(path)
 
 class TestParser:
     def test_parse_all_sch_files(self):
-        for root, dirs, files in os.walk(kicad_folder):
-
-            # don't go into any .git directories.
-            if ".git" in dirs:
-                dirs.remove(".git")
-
-            for file in files:
-                if file.endswith(".kicad_sch"):
-                    path = os.path.join(root, file)
-                    pro = Project(path)
-                    pro.parse()
+        assert len(kicad_sch_files) > 0
+        for path in kicad_sch_files:
+            pro = Project(path)
+            pro.parse()
 

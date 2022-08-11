f871b790dc35d05203435b6f71cd29c5cf6511c2
Kaspar Emanuel <kaspar@kitspace.org>
2022-08-11 14:27:19 +0100

    Remove unnecessary exclusion of .git dir in test

--- tests/test_parse_file_collection.py
+++ tests/test_parse_file_collection.py
@@ -14,15 +14,12 @@ kicad_folder = os.path.join(test_folder, "kicad_projects/kicad6-file-collection"
 
 kicad_pcb_files = []
 for root, dirs, files in os.walk(kicad_folder):
-    # don't go into any .git directories.
-    if ".git" in dirs:
-        dirs.remove(".git")
-
     for file in files:
         if file.endswith(".kicad_pcb"):
             path = os.path.join(root, file)
             kicad_pcb_files.append(path)
 
+print('len:', len(kicad_pcb_files))
 
 def test_parse_file_collection():
     assert len(kicad_pcb_files) > 0
